568c11418dbbc767527c81b2055e6a24b64212f8
theprogamer45 <juanpgh21@gmail.com>
2023-09-15 23:46:07 -0500

    rectangulo

---
+++ src/poo/pkg3/ejercicio/figuras/geometricas/rectangulo/POO3EjercicioFigurasGeometricasRectangulo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poo.pkg3.ejercicio.figuras.geometricas.rectangulo;
+
+/**
+ *
+ * @author juanp
+ */
+public class POO3EjercicioFigurasGeometricasRectangulo {
+
+    
+    public static void main(String[] args) {
+        Rectangulo form = new Rectangulo();
+        form.setVisible(true);
+    }
+    
+}
